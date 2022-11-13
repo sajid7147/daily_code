0a5eabb762fdd5526e361e32bf2a5e21ecc2de9d
sajid <sajidtp274@gmail.com>
2022-11-13 16:05:15 +0530

    added sum

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int n1,n2;
+  int sum;
+  printf("enter the first number ");
+  scanf("%d",&n1);
+  printf("enter the second number ");
+  scanf("%d",&n2);
+  sum=n1+n2;
+  printf("the result is %d",sum);
+
+}
