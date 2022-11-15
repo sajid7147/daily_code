a20e8838a7dce50554481077ce3d15dc769d00fb
sajid <sajidtp274@gmail.com>
2022-11-15 13:49:27 +0530

    Reverse string

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  char str[100],rev[100];
+  int i,j,len;
+  printf("Enter a string: ");
+  gets(str);
+  j=0;
+  for(i=0;str[i]!='\0';i++);
+  len=i;
+  for(i=len-1;i>=0;i--){
+    rev[j++]=str[i];}
+  
+  printf("\n the reversed string is %s",rev);
+
+}
