d240394201153548dd096e562566ae5e4efe2248
ashif p <93991186+azhiif@users.noreply.github.com>
2022-11-17 22:11:16 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-  int n1,n2;
-  int sum;
-  printf("enter the first number ");
-  scanf("%d",&n1);
-  printf("enter the second number ");
-  scanf("%d",&n2);
-  sum=n1+n2;
-  printf("the result is %d",sum);
+  int n1,n2; //  declaring numbers
+  int sum; // declaring integer sum
+  printf("enter the first number "); // showing to enter number
+  scanf("%d",&n1); // takes that number as n1
+  printf("enter the second number "); // showing print num2
+  scanf("%d",&n2); // taking that as n2
+  sum=n1+n2; // adding them and saving into sum
+  printf("the result is %d",sum); // showing the sum as output
 
 }
