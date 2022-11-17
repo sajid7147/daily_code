9ce1da4e68a782e053bcd3980a7990e4669de75a
sajid <sajidtp274@gmail.com>
2022-11-17 22:46:24 +0530

    Overload

---
+++ overload.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class overloadA {
+
+  void area(int a) {
+    System.out.println("The area of square is " + a * a + " sq units");
+  }
+
+  void area(float a, float b) {
+    System.out.println("The area of rectangle is " + a * b + " sq units");
+  }
+
+  void area(double a) {
+    System.out.println("The area of circle is " + 3.14 * a * a + " sq units");
+  }
+
+}
+
+public class overload {
+  public static void main(String args[])
+
+  {
+    overloadA ob = new overloadA();
+    ob.area(5);
+    ob.area(4, 10);
+    ob.area(2.25);
+  }
+}
